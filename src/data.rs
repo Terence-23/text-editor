842fba0d200b7aa47f1adad44421c2c63d249d50
terence-23 <jpuchalski1703@gmail.com>
2024-01-14 21:52:42 +0100

    Add arrow key handling

--- src/data.rs
+++ src/data.rs
@@ -1,20 +1,68 @@
-use std::path::{Path, PathBuf};
+use std::{cmp::min, path::PathBuf, sync::Arc};
+
+use parking_lot::RwLock;
 
 #[derive(Clone, Debug)]
 pub struct Line {
     pub data: String,
+    pub char_len: usize,
 }
 
 impl From<String> for Line {
     fn from(value: String) -> Self {
-        Line { data: value }
+        Line {
+            char_len: value.chars().count(),
+            data: value,
+        }
+    }
+}
+impl From<&str> for Line {
+    fn from(value: &str) -> Self {
+        Line {
+            data: value.to_owned(),
+            char_len: value.chars().count(),
+        }
     }
 }
 impl Line {
     fn new(txt: &str) -> Self {
-        Self {
-            data: txt.split('\n').next().unwrap_or("").to_owned(),
+        Self::from(txt.split('\n').next().unwrap_or(""))
+    }
+    pub fn remove(&mut self, idx: usize) {
+        self.char_len -= 1;
+        self.data.remove(idx);
+    }
+    pub fn split_at(&mut self, idx: usize) -> Line {
+        let remainder = self.data[idx..].to_owned();
+        self.data = self.data[..idx].to_owned();
+        self.char_len = self.data.chars().count();
+        Line::from(remainder)
+    }
+    pub fn push_str(&mut self, str: &str) {
+        let len = str.chars().count();
+        self.char_len += len;
+        self.data.push_str(str);
+    }
+    pub fn insert(&mut self, idx: usize, c: char) {
+        self.char_len += 1;
+        self.data.insert(idx, c);
+    }
+    pub fn len(&self) -> usize {
+        self.data.len()
+    }
+    pub fn get_char_pos(&self, idx: usize) -> usize {
+        if idx == 0 {
+            return 0;
         }
+        self.data.ceil_char_boundary(
+            self.data
+                .char_indices()
+                .take(idx)
+                .last()
+                .unwrap_or((0, '\0'))
+                .0
+                + 1,
+        )
     }
 }
 #[derive(Clone, Copy, Debug)]
@@ -25,16 +73,34 @@ pub struct FileData {
     pub path: PathBuf,
     pub location: TextPos,
     pub ended: bool,
+    pub size: TextPos,
+    pub top_visible: usize,
+    pub cursor_location: TextPos,
+}
+impl Drop for FileData {
+    fn drop(&mut self) {
+        dbg!(&self);
+        let start = self.top_visible;
+        let end = min(self.top_visible + self.size.0 + 1, self.lines.len());
+        for l in &self.lines[start..end] {
+            eprintln!("{}", l.data);
+        }
+    }
 }
 impl FileData {
     pub fn new() -> Self {
+        let (w, h) = crossterm::terminal::size().unwrap();
         Self {
             lines: vec![Line::new("")],
             path: PathBuf::default(),
+            cursor_location: TextPos(0, 0),
             location: TextPos(0, 0),
             ended: false,
+            size: TextPos(h.into(), w.into()),
+            top_visible: 0,
         }
     }
+    #[allow(unused)]
     pub fn get_next_and_prev_chars(&self) -> (usize, usize) {
         let line_text = &self.lines[self.location.0].data;
         (
@@ -49,5 +115,54 @@ impl FileData {
                 line_text.ceil_char_boundary(self.location.1 + 1)
             },
         )
+    }
+}
+
+#[derive(Clone, Debug)]
+pub struct SharedData {
+    data: Arc<RwLock<FileData>>,
+}
+
+impl Drop for SharedData {
+    fn drop(&mut self) {
+        eprintln!("Dropped file data");
+        if std::thread::panicking() {
+            eprintln!("panicking");
+            self.data.write().ended = true;
+        }
+    }
+}
+
+impl SharedData {
+    pub fn new() -> Self {
+        Self {
+            data: Arc::new(RwLock::new(FileData::new())),
+        }
+    }
+    pub fn get_next_and_prev_chars(&self) -> (usize, usize) {
+        let wdata = self.data.write();
+        let line_text = &wdata.lines[wdata.location.0].data;
+        (
+            if wdata.location.1 == 0 {
+                wdata.location.1
+            } else {
+                line_text.floor_char_boundary(wdata.location.1 - 1)
+            },
+            if wdata.location.1 == line_text.len() {
+                wdata.location.1
+            } else {
+                line_text.ceil_char_boundary(wdata.location.1 + 1)
+            },
+        )
+    }
+    pub fn write(
+        &self,
+    ) -> parking_lot::lock_api::RwLockWriteGuard<'_, parking_lot::RawRwLock, FileData> {
+        self.data.write()
+    }
+    pub fn read(
+        &self,
+    ) -> parking_lot::lock_api::RwLockReadGuard<'_, parking_lot::RawRwLock, FileData> {
+        self.data.read()
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -2,50 +2,71 @@
 
 mod data;
 
-use data::{Line, TextPos};
-use parking_lot::RwLock;
+use data::{Line, SharedData, TextPos};
+
 use std::{
+    cmp::min,
     io::{self, stdout, Write},
-    sync::Arc,
     thread,
     time::Duration,
 };
 
 use crossterm::{
-    cursor::MoveTo,
+    cursor::{self, Hide, MoveTo, Show},
     event::{read, KeyCode, KeyModifiers, KeyboardEnhancementFlags, PushKeyboardEnhancementFlags},
     execute, queue,
     style::Print,
     terminal::{Clear, ClearType},
 };
 
-async fn screen_refresh(data: Arc<RwLock<data::FileData>>) -> io::Result<()> {
+async fn screen_refresh(data: SharedData) -> io::Result<()> {
     let mut stdout = stdout();
     loop {
-        eprintln!("START");
-        queue!(stdout, Clear(ClearType::All))?;
-        queue!(stdout, MoveTo(0, 0))?;
+        queue!(stdout, Clear(ClearType::All), MoveTo(0, 0), Hide)?;
         let data = data.read();
         if data.ended {
             break;
         }
-        for l in &data.lines {
-            queue!(stdout, Print(l.data.to_owned() + "\r\n"))?;
-            eprintln!("{}", l.data)
+        let start = data.top_visible;
+        let end = min(data.top_visible + data.size.0, data.lines.len());
+        for (idx, l) in data.lines[start..end].iter().enumerate() {
+            queue!(
+                stdout,
+                Print(format!(
+                    "{:0>3}| {}",
+                    idx + start,
+                    l.data.to_owned() + if idx < data.size.0 - 1 { "\r\n" } else { "" }
+                ))
+            )?;
+            // eprintln!("{}", l.data)
+        }
+        for i in end - start..data.size.0 {
+            queue!(
+                stdout,
+                if i < data.size.0 - 1 {
+                    Print("~\r\n")
+                } else {
+                    Print("~")
+                }
+            )?;
         }
         queue!(
             stdout,
-            MoveTo(data.location.1 as u16, data.location.0 as u16)
+            MoveTo(
+                (data.cursor_location.1 + 5) as u16,
+                (data.location.0 - data.top_visible) as u16
+            ),
+            Show
         )?;
 
         stdout.flush()?;
         drop(data);
-        thread::sleep(Duration::from_millis(100));
+        thread::sleep(Duration::from_millis(10));
     }
     Ok(())
 }
 
-async fn event_loop(data: Arc<RwLock<data::FileData>>) -> io::Result<()> {
+async fn event_loop(data: SharedData) -> io::Result<()> {
     loop {
         let event = read()?;
         #[allow(unreachable_patterns)]
@@ -59,46 +80,139 @@ async fn event_loop(data: Arc<RwLock<data::FileData>>) -> io::Result<()> {
                     continue;
                 }
 
-                let (prev_char_bound, next_char_bound) = data.read().get_next_and_prev_chars();
+                let (prev_char_bound, next_char_bound) = data.get_next_and_prev_chars();
                 let mut w = data.write();
                 let pos = w.location;
+                assert_eq!(w.cursor_location.0, w.location.0, "Change before match");
                 match ke.code {
                     KeyCode::Backspace => {
                         if pos.1 == 0 && pos.0 > 0 {
                             // calculate new position of the cursor
-                            let n_pos = TextPos(pos.0 - 1, w.lines[pos.0 - 1].data.len());
+                            let n_pos = TextPos(pos.0 - 1, w.lines[pos.0 - 1].len());
                             let line = w.lines[pos.0].clone();
-                            w.lines[pos.0 - 1].data.push_str(&line.data); // append line contents
+                            w.cursor_location.1 = w.lines[pos.0 - 1].char_len;
+                            w.lines[pos.0 - 1].push_str(&line.data); // append line contents
                             w.lines.remove(pos.0); // remove old line
-                            w.location = n_pos
-                        } else {
-                            w.lines[pos.0].data.remove(prev_char_bound); // remove previous character
+                            w.location = n_pos;
+
+                            w.cursor_location.0 = pos.0 - 1
+                        } else if pos.1 > 0 {
+                            w.lines[pos.0].remove(prev_char_bound); // remove previous character
                             w.location.1 = prev_char_bound; // move cursor backward
+                            w.cursor_location.1 -= 1;
                         }
                     } // delete previous character if at start of line merge current into previous
                     KeyCode::Enter => {
-                        let remainder = w.lines[pos.0].data[pos.1..].to_owned();
-                        w.lines[pos.0].data = w.lines[pos.0].data[..pos.1].to_owned();
-                        w.lines.insert(pos.0 + 1, Line::from(remainder));
-                        w.location = TextPos(pos.0 + 1, 0);
+                        assert_eq!(w.cursor_location.0, w.location.0, "Change before enter");
+                        let new_line = w.lines[pos.0].split_at(pos.1);
+                        w.lines.insert(pos.0 + 1, new_line);
+                        w.location.1 = 0;
+                        w.cursor_location.1 = 0;
+                        w.location.0 += 1;
+                        w.cursor_location.0 += 1;
+                        assert_eq!(w.cursor_location.0, w.location.0, "Change after enter");
                     } // insert new line behind current
-                    KeyCode::Left => todo!(), // go to the left if at the start of the line go to end of previous
-                    KeyCode::Right => todo!(), // go to the right if at the end of the line go to start of next
-                    KeyCode::Up => todo!(),    // go one line up
-                    KeyCode::Down => todo!(),  // go one line down
-                    KeyCode::Home => todo!(),  // go to begin of line
-                    KeyCode::End => todo!(),   // go to end of line
-                    KeyCode::Tab => todo!(),   //insert tab
+                    KeyCode::Left => {
+                        if pos.1 > 0 {
+                            w.location.1 = prev_char_bound;
+                            w.cursor_location.1 -= 1;
+                        } else if pos.0 > 0 {
+                            w.location.0 -= 1;
+                            w.location.1 = w.lines[w.location.0].len();
+                            w.cursor_location.0 -= 1;
+                            w.cursor_location.1 = w.lines[w.location.0].char_len;
+                        }
+                    } // go to the left if at the start of the line go to end of previous
+                    KeyCode::Right => {
+                        if pos.1 < w.lines[pos.0].len() {
+                            w.location.1 = next_char_bound;
+                            w.cursor_location.1 += 1;
+                        } else if pos.0 < w.lines.len() - 1 {
+                            w.location.1 = 0;
+                            w.location.0 += 1;
+                            w.cursor_location = w.location.to_owned();
+                        }
+                    } // go to the right if at the end of the line go to start of next
+                    KeyCode::Up => {
+                        if pos.0 > 0 {
+                            w.location.0 -= 1;
+                            w.location.1 = w.lines[w.location.0].get_char_pos(w.cursor_location.1);
+                            eprintln!("{}", w.cursor_location.0);
+                            w.cursor_location.0 -= 1;
+                            w.cursor_location.1 =
+                                min(w.cursor_location.1, w.lines[w.location.0].char_len);
+                        }
+                    } // go one line up
+                    KeyCode::Down => {
+                        if pos.0 < w.lines.len() - 1 {
+                            w.location.0 += 1;
+                            w.location.1 = w.lines[w.location.0].get_char_pos(w.cursor_location.1);
+                            w.cursor_location.0 += 1;
+                            w.cursor_location.1 =
+                                min(w.lines[w.location.0].char_len, w.cursor_location.1);
+                        }
+                    } // go one line down
+                    KeyCode::PageDown => {
+                        w.location.0 += w.size.0;
+                        w.cursor_location.0 += w.size.0;
+                        if w.location.0 >= w.lines.len() {
+                            w.location.0 = w.lines.len() - 1;
+                            w.cursor_location.0 = w.lines.len() - 1;
+                        }
+                        w.location.1 = w.lines[w.location.0].get_char_pos(w.cursor_location.1);
+                        w.cursor_location.1 =
+                            min(w.lines[w.location.0].char_len, w.cursor_location.1);
+                    }
+                    KeyCode::PageUp => {
+                        if pos.0 > w.size.0 {
+                            w.location.0 -= w.size.0;
+                            w.cursor_location.0 -= w.size.0;
+                        } else {
+                            w.location.0 = 0;
+                            w.cursor_location.0 = 0;
+                        }
+                        w.location.1 = w.lines[w.location.0].get_char_pos(w.cursor_location.1);
+                        w.cursor_location.1 =
+                            min(w.lines[w.location.0].char_len, w.cursor_location.1);
+                    }
+                    KeyCode::Home => {
+                        w.location.1 = 0;
+                        w.cursor_location.1 = 0;
+                    } // go to begin of line
+                    KeyCode::End => {
+                        w.location.1 = w.lines[w.location.0].len();
+                        w.cursor_location.1 = w.lines[w.location.0].char_len;
+                    } // go to end of line
+                    KeyCode::Tab => todo!(),     //insert tab
                     KeyCode::BackTab => todo!(), // delete tab from the start of the current line
-                    KeyCode::Delete => todo!(), // delete next character if at end of line merge the next one into current
+                    KeyCode::Delete => {
+                        if pos.1 == w.lines[pos.0].len() && pos.0 < w.lines.len() - 1 {
+                            let data = w.lines[pos.0 + 1].data.to_owned();
+                            w.lines[pos.0].push_str(&data);
+                            w.lines.remove(pos.0 + 1);
+                        } else if pos.1 != w.lines[pos.0].len() {
+                            w.lines[pos.0].remove(pos.1);
+                        }
+                    } // delete next character if at end of line merge the next one into current
                     KeyCode::Char(c) => {
-                        w.lines[pos.0].data.insert(pos.1, c);
-                        w.location.1 += c.len_utf8()
+                        w.lines[pos.0].insert(pos.1, c);
+                        w.location.1 += c.len_utf8();
+                        w.cursor_location.1 += 1;
                     } // insert character and advance the character pointer by 1
                     KeyCode::Null => todo!(),
                     KeyCode::Esc => todo!(),
                     _ => {}
                 }
+                if w.location.0 < w.top_visible {
+                    w.top_visible = w.location.0
+                } else if w.location.0 >= w.top_visible + w.size.0 {
+                    w.top_visible = w.location.0 - w.size.0 + 1
+                }
+                assert_eq!(w.cursor_location.0, w.location.0, "Change")
+            }
+            crossterm::event::Event::Resize(w, h) => {
+                data.write().size = TextPos(h.into(), w.into());
+                eprintln!("RESIZED");
             }
             _ => {}
             crossterm::event::Event::FocusGained => todo!(),
@@ -106,7 +220,6 @@ async fn event_loop(data: Arc<RwLock<data::FileData>>) -> io::Result<()> {
 
             crossterm::event::Event::Mouse(_) => todo!(),
             crossterm::event::Event::Paste(_) => todo!(),
-            crossterm::event::Event::Resize(_, _) => todo!(),
         }
     }
     Ok(())
@@ -114,19 +227,19 @@ async fn event_loop(data: Arc<RwLock<data::FileData>>) -> io::Result<()> {
 
 #[tokio::main]
 async fn main() -> io::Result<()> {
-    println!("Hello, world!");
     crossterm::terminal::enable_raw_mode()?;
     crossterm::execute!(
         stdout(),
         PushKeyboardEnhancementFlags(KeyboardEnhancementFlags::DISAMBIGUATE_ESCAPE_CODES)
-    );
-    let fdata = Arc::new(RwLock::new(data::FileData::new()));
+    )?;
+    let fdata = SharedData::new();
     let event_handle = tokio::spawn(event_loop(fdata.clone()));
     tokio::spawn(screen_refresh(fdata.clone()));
 
     // refresh_handle.await?;
     event_handle.await??;
     crossterm::terminal::disable_raw_mode()?;
+    execute!(stdout(), Clear(ClearType::All), Show)?;
 
     Ok(())
 }
